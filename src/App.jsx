cbfaa423bfea111992e5fe16c4b316131d7a3b78
chlobg <136447267+chlobg@users.noreply.github.com>
2025-08-15 10:38:11 +0700

    Correct import path for PortFolio component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
-import PortFolio from "./components/Portfolio";
+import PortFolio from "./components/PortFolio";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
